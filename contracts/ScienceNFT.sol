ad95f18ddd8726aa4f6b32aba967827789f20447
Aakaash Meduri <aakaash.meduri@gmail.com>
2022-04-22 23:03:58 +0200

    Science NFT contract with event emissions

---
+++ contracts/ScienceNFT.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ScienceNFT is ERC721, ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Science NFT", "SNFT") {}
+
+    function _baseURI() internal pure override returns(string memory) {
+        return "ipfs://";
+    }
+
+    event tokenMinted(uint256 indexed _tokenId, string _tokenURI, address _mintAddress);
+
+    function safeMint(address _to, string memory _tokenURI) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(_to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        emit tokenMinted(tokenId, _tokenURI, _to);
+    }
+
+    function _burn(uint256 tokenId) internal override (ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns(string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
